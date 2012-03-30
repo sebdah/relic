bca658b3e71e599451951eb0e4481a7e34a0dc5c
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-03-30 23:29:51 +0200

    Move flash message handling to site wide level

--- jeeves/media/js/script.js
+++ jeeves/media/js/script.js
@@ -1,7 +1,15 @@
-/* Author:
-
+/*
+Handle message box
 */
-
+$(document).ready(function(){
+    if ($("#message").text().length < 1) {
+        $("#message").hide();
+    } else {
+        $("#message").fadeOut(10000, function(){ 
+            $("#message").remove()
+        });
+    }
+});
 
 
 

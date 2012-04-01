5d9401f6cf177b1e06d4b30e9dc449984dd3cd52
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-04-01 17:30:29 +0200

    Wrote simple forms for all new models

--- jeeves/cloud/forms.py
+++ jeeves/cloud/forms.py
@@ -6,3 +6,28 @@ class CloudForm(ModelForm):
     class Meta:
         model = models.Cloud
         fields = ('name', 'aws_id', 'aws_secret')
+
+class RoleForm(ModelForm):
+    class Meta:
+        model = models.Role
+        fields = ('name', 'cloud')
+
+class InstanceForm(ModelForm):
+    class Meta:
+        model = models.Instance
+        fields = ('role', 'hostname', 'instance_type', 'availability_zone')
+
+class Package(ModelForm):
+    class Meta:
+        model = models.Package
+        field = ('name', 'role')
+
+class EBSVolume(ModelForm):
+    class Meta:
+        model = models.EBSVolume
+        fields = ('mountpoint', 'size', 'role')
+
+class ElasticIP(ModelForm):
+    class Meta:
+        model = models.ElasticIP
+        fields = ('dns_name', 'role')

c145e21e219545a6ed99a5718aa3518a447a777e
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-04-01 17:25:16 +0200

    Wrote models for instances, roles, packages, ebsvols and elastic ips

---
+++ jeeves/cloud/definitions.py
@@ -0,0 +1,33 @@
+AVAILABILITY_ZONES = (
+    ('ap-northeast-1a', 'ap-northeast-1a'),
+    ('ap-northeast-1b', 'ap-northeast-1b'),
+    ('ap-southeast-1a', 'ap-southeast-1a'),
+    ('ap-southeast-1b', 'ap-southeast-1b'),
+    ('eu-west-1a', 'eu-west-1a'),
+    ('eu-west-1b', 'eu-west-1b'),
+    ('eu-west-1c', 'eu-west-1c'),
+    ('sa-east-1a', 'sa-east-1a'),
+    ('sa-east-1b', 'sa-east-1b'),
+    ('us-east-1a', 'us-east-1a'),
+    ('us-east-1b', 'us-east-1b'),
+    ('us-east-1c', 'us-east-1c'),
+    ('us-east-1d', 'us-east-1d'),
+    ('us-east-1e', 'us-east-1e'),
+    ('us-west-1a', 'us-west-1a'),
+    ('us-west-1b', 'us-west-1b'),
+    ('us-west-1c', 'us-west-1b'),
+    ('us-west-2a', 'us-west-2a'),
+    ('us-west-2b', 'us-west-2b'),
+)
+
+INSTANCE_TYPES = (
+    ('m1.small',    'Standard - Small - 1.7 GB memory - 1 core, 1 EC2 CU - 64 bit'),
+    ('m1.medium',   'Standard - Medium - 3.75 GB memory - 1 core, 2 EC2 CU each - 64 bit'),
+    ('m1.large',    'Standard - Large - 7.5 GB memory - 2 cores, 2 EC2 CU each - 64 bit'),
+    ('m1.xlarge',   'Standard - Extra Large - 15 GB memory - 4 cores, 2 EC2 CU each - 64 bit'),
+    ('m2.xlarge',   'High Mem - Extra Large - 17.1 GB memory - 2 cores, 3.25 EC2 CU each - 64 bit'),
+    ('m2.2xlarge',  'High Mem - Double Extra Large - 34.2 GB memory - 4 cores, 3.25 EC2 CU each - 64 bit'),
+    ('m2,4xlarge',  'High Mem - Quadruple Extra Large - 68.4 GB memory - 8 cores, 3.25 EC2 CU each - 64 bit'),
+    ('c1.medium',   'High CPU - Medium - 1.7 GB memory - 2 cores, 2.5 EC2 CU each - 64 bit'),
+    ('c1.xlarge',   'High CPU - Extra Large - 7 GB memory - 8 cores, 2.5 EC2 CU each - 64 bit'),
+)
--- jeeves/cloud/models.py
+++ jeeves/cloud/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from core.models import Account
+from boto import ec2
+from cloud import definitions
 
 class Cloud(models.Model):
     """
@@ -20,3 +22,62 @@ class Cloud(models.Model):
     created             = models.DateTimeField(auto_now_add = True)
     last_updated        = models.DateTimeField(auto_now = True)
     is_active           = models.BooleanField(default = True)
+
+class Role(models.Model):
+    """
+    Defines a Role
+    """
+    def __unicode__(self):
+        return self.name
+
+    name                = models.CharField(blank = False, max_length = 30)
+    cloud               = models.ForeignKey(Cloud)
+
+class Instance(models.Model):
+    """
+    Definition of an instance
+    """
+    def __unicode__(self):
+        return self.hostname
+    
+    role                = models.ForeignKey(Role)
+    hostname            = models.CharField( blank = False,
+                                            max_length = 30)
+    instance_id         = models.CharField(max_length = 10)
+    instance_type       = models.CharField( blank = False,
+                                            max_length = 10,
+                                            choices = definitions.INSTANCE_TYPES)
+    availability_zone   = models.CharField( blank = False,
+                                            max_length = 10,
+                                            choices = definitions.AVAILABILITY_ZONES)
+
+class Package(models.Model):
+    """
+    Defines a OS package
+    """
+    def __unicode__(self):
+        return self.name
+    
+    name                = models.CharField(blank = False, max_length = 60)
+    role                = models.ForeignKey(Role)
+
+class EBSVolume(models.Model):
+    """
+    Define an EBS volume
+    """
+    def __unicode__(self):
+        return "%s (%i GB)" % (self.mountpoint, self.size)
+    
+    mountpoint          = models.CharField(blank = False, max_length = 250)
+    size                = models.IntegerField(blank = False, default = 10)
+    role                = models.ForeignKey(Role)
+
+class ElasticIP(models.Model):
+    """
+    Definition of an Elastic IP
+    """
+    def __unicode__(self):
+        return self.dns_name
+    
+    dns_name            = models.CharField(blank = False, max_length = 250)
+    role                = models.ForeignKey(Role)

7c709df69fd49b926fdeb0e439d76b26483e4e1e
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-09-22 08:37:49 +0200

    Big clean up of old crap.

--- jeeves/cloud/admin.py
+++ jeeves/cloud/admin.py
@@ -1,33 +1,8 @@
 from cloud import models
 from django.contrib import admin
 
+
 class CloudAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'name', 'owner')
 
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_global')
-    list_filter = ('is_global',)
-
-class RoleRelationAdmin(admin.ModelAdmin):
-    list_display = ('cloud', 'role')
-
-class InstanceAdmin(admin.ModelAdmin):
-    list_display = ('role', 'hostname', 'instance_id', 'availability_zone')
-    list_filter = ('availability_zone',)
-    
-class PackageAdmin(admin.ModelAdmin):
-    list_display = ('role', 'name')
-    
-class EBSVolumeAdmin(admin.ModelAdmin):
-    list_display = ('instance', 'mountpoint', 'size')
-    
-class ElasticIPAdmin(admin.ModelAdmin):
-    list_display = ('instance', 'public_ip')
-
 admin.site.register(models.Cloud, CloudAdmin)
-admin.site.register(models.Role, RoleAdmin)
-admin.site.register(models.RoleRelation, RoleRelationAdmin)
-admin.site.register(models.Instance, InstanceAdmin)
-admin.site.register(models.Package, PackageAdmin)
-admin.site.register(models.EBSVolume, EBSVolumeAdmin)
-admin.site.register(models.ElasticIP, ElasticIPAdmin)

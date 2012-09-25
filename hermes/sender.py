9a31fc032e76f8ccef4771b646c49e2770a43bdc
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-09-25 09:52:10 +0200

    Implemented argparse for Hermes sender

--- hermes/sender.py
+++ hermes/sender.py
@@ -6,21 +6,58 @@ Command line utility to send messages to RabbitMQ
 
 import sys
 import pika
+import argparse
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
 
-channel.queue_declare(queue='task_queue', durable=True)
+def main():
+    """
+    Main function
+    """
+    parser = argparse.ArgumentParser(
+        conflict_handler="resolve", 
+        description="Send commands to RabbitMQ")
+    parser.add_argument(
+        '-m', '--message', action='store', type=str, 
+        dest='message', default='',
+        help='Message to send (required)')
+    parser.add_argument(
+        '-h', '--host', action='store', type=str, 
+        dest='host', default='localhost',
+        help='Hostname for RabbitMQ')
+    parser.add_argument(
+        '-p', '--port', action='store', type=int,
+        dest='port', default=5672,
+        help='Port for RabbitMQ')
+    parser.add_argument(
+        '-q', '--queue', action='store', type=str,
+        dest='queue', default='task_queue',
+        help='Queue name for the RabbitMQ queue')
+    args = parser.parse_args()
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange='',
-                      routing_key='task_queue',
-                      body=message,
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-print " [x] Sent %r" % (message,)
-connection.close()
+    # Check that we got a message
+    if args.message is '':
+        print "Error: --message must be set\n"
+        parser.print_help()
+        sys.exit(1)
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host=args.host,
+            port=args.port))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=args.queue, durable=True)
+
+    channel.basic_publish(exchange='',
+                          routing_key=args.queue,
+                          body=args.message,
+                          properties=pika.BasicProperties(
+                             delivery_mode = 2, # make message persistent
+                          ))
+    print " [x] Sent %r" % (args.message,)
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
 
 sys.exit(0)
